077a610f6d6267b18e2bd0ddb15bbdc417d3059f
bulyan <38100527+boelchan@users.noreply.github.com>
2026-03-03 11:34:03 +0700

    up

--- resources/views/pages/⚡dashboard/dashboard.blade.php
+++ resources/views/pages/⚡dashboard/dashboard.blade.php
@@ -130,10 +130,10 @@
                                 <i class="ti ti-hash"></i>{{ $inc->no_antrian }} • {{ $inc->patient->medical_record_number }} • {{ $inc->patient->village?->name }}
                             </div>
                         </div>
-                        <a class="btn btn-warning btn-square"
+                        <a class="btn btn-warning btn-square btn-md"
                             href="{{ route('encounter.edit', [$inc->id, $inc->uuid]) }}" 
                         >
-                            <i class="ti ti-chevron-right text-lg"></i>
+                            <i class="ti ti-stethoscope text-2xl font-bold"></i>
                         </a>
                     </div>
                 @empty
